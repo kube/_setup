a2c9002324f5bf955fadc6e9a4c5e6793e97f7d9
Chris Feijoo <chris@kube.io>
2021-11-30 22:14:13 +0100

    Save new homebrew bin folder

--- mac/fish.sh
+++ mac/fish.sh
@@ -1,3 +1,3 @@
 brew install fish
-sudo sh -c 'echo /usr/local/bin/fish >> /etc/shells'
-chsh -s /usr/local/bin/fish
+sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
+chsh -s /opt/homebrew/bin/fish
