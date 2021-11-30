2d8fd71a5b58a8b5fc8c0884e7dec1ba8af55f11
Chris Feijoo <chris@kube.io>
2021-11-30 22:18:11 +0100

    Update config.sh on install

--- mac/fish.sh
+++ mac/fish.sh
@@ -1,3 +1,5 @@
 brew install fish
 sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
 chsh -s /opt/homebrew/bin/fish
+
+echo "set PATH /opt/homebrew/bin \$PATH" >> "~/.config/fish/config.fish"
